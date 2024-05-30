8fd8f45715394aa51f903ef1f5bf61bb01ff116a
Aydily <aydilyodettemorales@gmail.com>
2024-05-29 20:58:23 -0600

    Text Area

--- app/helpers/agregarBlog.js
+++ app/helpers/agregarBlog.js
@@ -3,7 +3,9 @@ const formAgregarBlog = document.querySelector(".formulario");
 formAgregarBlog.addEventListener("submit", function (e) {
   e.preventDefault();
   const titulo = document.querySelector("#name").value;
+  const contenido = document.querySelector("#msg").value;
   console.log(titulo);
+  console.log(contenido);
 
   // agregarBlog(titulo);
   formAgregarBlog.reset();

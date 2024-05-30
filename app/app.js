84e7a48f0143126f17dbe919b79551bd2235fb1a
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-05-29 20:01:05 -0600

    chore: Update navbar and footer in HTML pages

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import { itemBlogs } from "./component/ItemBlogs.js";
 import { eliminarBlog } from "./helpers/eliminarBlog.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  cargarNavbar("/pages/navbar.html");
+  cargarNavbar();
   const contentBlog = document.querySelector(".items_blog");
   itemBlogs(contentBlog);
   eliminarBlog();
@@ -10,21 +10,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // JavaScript para hacer el navbar reutilizable
 function cargarNavbar() {
-    fetch('navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navbar').innerHTML = data;
-      });
-  }
-  
-  // JavaScript para hacer el navbar reutilizable
-  document.addEventListener('DOMContentLoaded', cargarNavbar);
- 
-    document.addEventListener("DOMContentLoaded", function() {
-      fetch('/pages/footer.html')
-        .then(response => response.text())
-        .then(data => {
-          document.querySelector('footer').innerHTML = data;
-        });
+  fetch("/pages/navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
     });
- 
+}
+
+// JavaScript para hacer el navbar reutilizable
+document.addEventListener("DOMContentLoaded", cargarNavbar);
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/pages/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector("footer").innerHTML = data;
+    });
+});

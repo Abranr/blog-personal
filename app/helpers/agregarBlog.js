71c2536317252f5a28c65cb77b21a14bc731708a
Abranr <abran2@miumg.edu.gt>
2024-05-31 17:47:33 -0600

    agregue el blog al local storage

--- app/helpers/agregarBlog.js
+++ app/helpers/agregarBlog.js
@@ -7,15 +7,30 @@ formAgregarBlog.addEventListener("submit", function (e) {
   console.log(titulo);
   console.log(contenido);
 
-  // const data = {
-  //   id: // Generar un id único, // Rodrigo
-  //   titulo: // Obtener el valor del input de titulo, // Andre
-  //   contenido: // Obtener el valor del input de contenido, // Andre
-  // };
+  // Generar un id único
+  const id = Date.now().toString();
 
-  // agregarBlog(data);
+  // Crear el objeto data con los valores del formulario
+  const data = {
+    id: id,
+    titulo: titulo,
+    contenido: contenido,
+  };
+
+  // Llamar a la función agregarBlog con el objeto data
+  agregarBlog(data);
   formAgregarBlog.reset();
 });
+
 export function agregarBlog(data) {
-  // Andre agregar el blog al localStorage
+  // Obtener los blogs existentes del localStorage
+  let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
+
+  // Agregar el nuevo blog a la lista de blogs
+  blogs.push(data);
+
+  // Guardar la lista actualizada de blogs en el localStorage
+  localStorage.setItem("blogs", JSON.stringify(blogs));
+
+  console.log("Blog agregado:", data);
 }

22906221402d0c7374d0916096e9c6a87b9cbf2a
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-06-02 19:11:37 -0600

    chore: Arreglar el boton de editar, al dar click crea un boton guardar

--- app/helpers/editarBlog.js
+++ app/helpers/editarBlog.js
@@ -2,12 +2,18 @@ const btnEditar = document.querySelector(".Btn");
 const titulo = document.querySelector(".Nombre-blog");
 const contenido = document.querySelector("#blogContent");
 export const editarBlog = () => {
-  btnEditar.addEventListener("click", (e) => {
+  btnEditar.addEventListener("click", () => {
     titulo.removeAttribute("disabled");
     contenido.removeAttribute("disabled");
+
+    // si ya existe un boton de guardar no lo vuelvas a crear
+    if (document.querySelector(".guardar")) {
+      return;
+    }
     const btnGuardar = document.createElement("button");
     btnGuardar.textContent = "Guardar";
     btnGuardar.classList.add("Btn");
+    btnGuardar.classList.add("guardar");
     btnGuardar.addEventListener("click", () => {
       const idBlog = localStorage.getItem("mostrarBlog");
       const dataBlogs = JSON.parse(localStorage.getItem("blogs"));
@@ -22,6 +28,7 @@ export const editarBlog = () => {
       contenido.setAttribute("disabled", true);
       btnGuardar.remove();
     });
+
     btnEditar.after(btnGuardar);
   });
 };
